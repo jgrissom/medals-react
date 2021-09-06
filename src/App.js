f071ae7166a7569cc9a6198e0536899117ba02c7
jgrissom <jgrissom91@gmail.com>
2021-09-06 14:15:44 -0500

    [6.02] Fetch data using Axios
    - Import Axios into App component
    - Using the "componentDidMount" useEffect, asynchronously load the countries into state

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@
 // Author:      Jeff Grissom
 // Version:     6.xx
 import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import Country from './components/Country';
 import NewCountry from './components/NewCountry';
 import './App.css';
 
 const App = () => {
+  const apiEndpoint = "https://localhost:5001/api/country";
+  // const apiEndpoint = "https://medalsapi.azurewebsites.net/api/country";
   const [ countries, setCountries ] = useState([]);
   const medals = useRef([
     { id: 1, name: 'gold' },
@@ -17,20 +20,19 @@ const App = () => {
   // this is the functional equivalent to componentDidMount
   useEffect(() => {
     // initial data loaded here
-    let fetchedCountries = [
-      { id: 1, name: 'United States', gold: 2, silver: 2, bronze: 3 },
-      { id: 2, name: 'China', gold: 3, silver: 1, bronze: 0 },
-      { id: 3, name: 'Germany', gold: 0, silver: 2, bronze: 2 },
-    ]
-    setCountries(fetchedCountries);
+    async function fetchCountries() {
+      const { data : fetchedCountries } = await axios.get(apiEndpoint);
+      setCountries(fetchedCountries);
+    }
+    fetchCountries();
   }, []);
 
   const handleAdd = (name) => {
-    const id = countries.length === 0 ? 1 : Math.max(...countries.map(country => country.id)) + 1;
-    setCountries([...countries].concat({ id: id, name: name, gold: 0, silver: 0, bronze: 0 }));
+    // const id = countries.length === 0 ? 1 : Math.max(...countries.map(country => country.id)) + 1;
+    // setCountries([...countries].concat({ id: id, name: name, gold: 0, silver: 0, bronze: 0 }));
   }
   const handleDelete = (countryId) => {
-    setCountries([...countries].filter(c => c.id !== countryId));
+    // setCountries([...countries].filter(c => c.id !== countryId));
   }
   const handleIncrement = (countryId, medalName) => {
     const idx = countries.findIndex(c => c.id === countryId);
