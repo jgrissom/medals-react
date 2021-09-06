95d9ad2e5c33161fee4ac7a59a8254a16e360302
jgrissom <jgrissom91@gmail.com>
2021-09-06 17:28:18 -0500

    [7.05] Use conditional rendering to highlight unsaved medal counts

--- src/components/Medal.jsx
+++ src/components/Medal.jsx
@@ -4,7 +4,12 @@ const Medal = (props) => {
   const { medal, country, onIncrement, onDecrement } = props;
   return (
     <div className="medals">
-      { medal.name } Medals: { country[medal.name].page_value }
+      { 
+        ( country[medal.name].page_value !== country[medal.name].saved_value) ?
+          <span className="delta">{medal.name} Medals: {country[medal.name].page_value}</span>
+        :
+          <span>{medal.name} Medals: {country[medal.name].page_value}</span>
+      }
       <button onClick={ () => onIncrement(country.id, medal.name) }>+</button>
       <button disabled={ country[medal.name].page_value === 0 } onClick={ () => onDecrement(country.id, medal.name) }>-</button>
     </div>
