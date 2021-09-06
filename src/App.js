c9c5e85ba4ff2154440fc7a5a6b79ad3c977ad2d
jgrissom <jgrissom91@gmail.com>
2021-09-06 17:51:50 -0500

    [8.07] ReceiveDeleteMessage - update state when message received

--- src/App.js
+++ src/App.js
@@ -80,6 +80,9 @@ const App = () => {
 
         connection.on('ReceiveDeleteMessage', id => {
           console.log(`Delete id: ${id}`);
+          let mutableCountries = [...latestCountries.current];
+          mutableCountries = mutableCountries.filter(c => c.id !== id);
+          setCountries(mutableCountries);
         });
       })
       .catch(e => console.log('Connection failed: ', e));
