066825ffe2eafd7ddd59582b313c7f2ed51be63c
jgrissom <jgrissom91@gmail.com>
2021-10-01 08:47:27 -0500

    [9.16] Logout

--- src/App.js
+++ src/App.js
@@ -223,6 +223,18 @@ const App = () => {
       }
     }
   }
+  const handleLogout = (e) => {
+    e.preventDefault();
+    console.log('logout');
+    localStorage.removeItem('token');
+    setUser({
+      name: null,
+      canPost: false,
+      canPatch: false,
+      canDelete: false
+    });
+    return false;
+  }
   const getUser = (encodedJwt) => {
     const decodedJwt = jwtDecode(encodedJwt);
     return {
@@ -244,7 +256,11 @@ const App = () => {
         <span className='badge'>
           { getAllMedalsTotal() }
         </span>
-        <Link to="/login" className='loginLink'>Login</Link>
+        {user.name ? 
+          <span className='logout'><a href="/" onClick={handleLogout} className='logoutLink'>Logout</a> [{user.name}]</span>
+          :
+          <Link to="/login" className='loginLink'>Login</Link>
+        }
       </div>
       <Route exact path="/login">
         <Login onLogin={handleLogin} />
