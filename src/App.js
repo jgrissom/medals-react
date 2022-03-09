fe19d75370df5d56286c108b2ac3b793a633334c
jgrissom <jgrissom91@gmail.com>
2022-03-09 12:54:51 -0600

    [9.11] handleLogin - axios post

--- src/App.js
+++ src/App.js
@@ -14,9 +14,11 @@ const App = () => {
   // const apiEndpoint = "https://localhost:5001/api/country";
   const apiEndpoint = "https://localhost:5001/jwt/api/country";
   const hubEndpoint = "https://localhost:5001/medalsHub"
+  const usersEndpoint = "https://localhost:5001/api/users/login";
   // const apiEndpoint = "https://medalsapi.azurewebsites.net/api/country";
   // const apiEndpoint = "https://medalsapi.azurewebsites.net/jwt/api/country";
   // const hubEndpoint = "https://medalsapi.azurewebsites.net/medalsHub"
+  // const usersEndpoint = "https://medalsapi.azurewebsites.net/api/users/login";
   const [ countries, setCountries ] = useState([]);
   const [ connection, setConnection] = useState(null);
   const medals = useRef([
@@ -147,7 +149,19 @@ const App = () => {
     }
   }
   const handleLogin = async (username, password) => {
-    console.log(`login: ${username}`);
+    try {
+      const resp = await axios.post(usersEndpoint, { username: username, password: password });
+      const encodedJwt = resp.data.token;
+      console.log(encodedJwt);
+    } catch (ex) {
+      if (ex.response && (ex.response.status === 401 || ex.response.status === 400 )) {
+        alert("Login failed");
+      } else if (ex.response) {
+        console.log(ex.response);
+      } else {
+        console.log("Request failed");
+      }
+    }
   }
   const getAllMedalsTotal = () => {
     let sum = 0;
