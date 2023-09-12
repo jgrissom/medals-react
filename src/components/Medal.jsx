776a3b0ba4fa74fd0c274bff8ca97d948113bade
jgrissom <jgrissom91@gmail.com>
2023-09-11 19:46:12 -0500

    [4a.10] Medal Badge
    npm i react-bootstrap-icons@1.10.3

--- src/components/Medal.jsx
+++ src/components/Medal.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import Badge from 'react-bootstrap/Badge';
+
 
 class Medal extends Component {
   state = {  }
   render() { 
     const { medal, country, onIncrement, onDecrement } = this.props;
     return (
-      <div className="medals">
-        { medal.name } Medals: { country[medal.name] }
-        <button onClick={ () => onIncrement(country.id, medal.name) }>+</button>
-        <button disabled={ country[medal.name] === 0 } onClick={ () => onDecrement(country.id, medal.name) }>-</button>
-      </div>
+      <React.Fragment>
+        <div style={{ textTransform: "capitalize"}}>
+          { medal.name } Medals
+        </div>
+        <div className="medal-count">
+          <Badge bg="primary" text="light">
+            { country[medal.name] }
+          </Badge>
+        </div>
+      </React.Fragment>
     );
   }
 }
