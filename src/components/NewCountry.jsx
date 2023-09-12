bc89411b29397dcea76f16d1c6b244f2165049a6
jgrissom <jgrissom91@gmail.com>
2023-09-11 20:15:32 -0500

    [4a.18] New Country Form

--- src/components/NewCountry.jsx
+++ src/components/NewCountry.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
 import { PlusCircleFill } from 'react-bootstrap-icons';
 
 class NewCountry extends Component {
   state = {
     showModal: false,
   }
+  handleModalClose = () => this.setState({ showModal: false });
 
   render() { 
     return (
@@ -14,13 +16,23 @@ class NewCountry extends Component {
         <Button variant="outline-success" onClick={ () => this.setState({ showModal: true })}>
           <PlusCircleFill />
         </Button>
-        <Modal show={ this.state.showModal }>
+        <Modal show={ this.state.showModal } onHide={ this.handleModalClose }>
           <Modal.Header closeButton>
             <Modal.Title>New Country</Modal.Title>
           </Modal.Header>
-          <Modal.Body>[ New Country Info Here ]</Modal.Body>
+          <Modal.Body>
+            <Form.Group className="mb-3" controlId="modalForm1">
+              <Form.Label>Country Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="enter name"
+                autoFocus
+                autoComplete='off'
+              />
+            </Form.Group>
+          </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary">
+            <Button variant="secondary" onClick={ this.handleModalClose }>
               Close
             </Button>
             <Button variant="primary">
