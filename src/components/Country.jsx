fe551d224c464d5a61a115268bfba5744098dffd
jgrissom <jgrissom91@gmail.com>
2023-09-11 21:23:31 -0500

    [5.03] Convert Country to functional component

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,45 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Medal from './Medal';
 import { TrashFill } from 'react-bootstrap-icons';
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
 import ListGroup from 'react-bootstrap/ListGroup';
 
-class Country extends Component {
-  getMedalsTotal(country, medals) {
+const Country = (props) => {
+  const { country, medals, onIncrement, onDecrement, onDelete } = props;
+  const getMedalsTotal = (country, medals) => {
     let sum = 0;
     medals.forEach(medal => { sum += country[medal.name]; });
     return sum;
   }
-  render() { 
-    const { country, medals, onIncrement, onDecrement, onDelete } = this.props;
-    return (
-      <Card>
-        <Card.Body>
-          <Card.Title className="d-flex justify-content-between">
-            <span>
-            { country.name }
-            <Badge bg="secondary" text="light" pill className="ml-2">
-              { this.getMedalsTotal(country, medals) }
-            </Badge>
-            </span>
-            <TrashFill onClick={() => onDelete(country.id)} className='icon-btn' style={{ color:'red' }} />
-          </Card.Title>
-          <ListGroup variant="flush">
-          { medals.map(medal =>
-          <ListGroup.Item className="d-flex justify-content-between" key={ medal.id }>
-            <Medal  
-              country={ country } 
-              medal={ medal } 
-              onIncrement={ onIncrement } 
-              onDecrement={ onDecrement } />
-          </ListGroup.Item>
-          ) }
-          </ListGroup>
-        </Card.Body>
-      </Card>
-    );
-  }
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Title className="d-flex justify-content-between">
+          <span>
+          { country.name }
+          <Badge bg="secondary" text="light" pill className="ml-2">
+            { getMedalsTotal(country, medals) }
+          </Badge>
+          </span>
+          <TrashFill onClick={() => onDelete(country.id)} className='icon-btn' style={{ color:'red' }} />
+        </Card.Title>
+        <ListGroup variant="flush">
+        { medals.map(medal =>
+        <ListGroup.Item className="d-flex justify-content-between" key={ medal.id }>
+          <Medal  
+            country={ country } 
+            medal={ medal } 
+            onIncrement={ onIncrement } 
+            onDecrement={ onDecrement } />
+        </ListGroup.Item>
+        ) }
+        </ListGroup>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default Country;
