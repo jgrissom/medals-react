79108c9773a9bb5ef21e6407b05fe85e8ecda9e8
jgrissom <jgrissom91@gmail.com>
2023-09-11 20:08:09 -0500

    [4a.17] New Country Modal

--- src/components/NewCountry.jsx
+++ src/components/NewCountry.jsx
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 import { PlusCircleFill } from 'react-bootstrap-icons';
 
 class NewCountry extends Component {
-  state = {  }
+  state = {
+    showModal: false,
+  }
 
   render() { 
     return (
       <React.Fragment>
-        <Button variant="outline-success">
+        <Button variant="outline-success" onClick={ () => this.setState({ showModal: true })}>
           <PlusCircleFill />
         </Button>
+        <Modal show={ this.state.showModal }>
+          <Modal.Header closeButton>
+            <Modal.Title>New Country</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>[ New Country Info Here ]</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary">
+              Close
+            </Button>
+            <Button variant="primary">
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </React.Fragment>
     );
   }
