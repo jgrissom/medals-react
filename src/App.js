2760af93b450fc4a94030970916c200eb295bfc3
jgrissom <jgrissom91@gmail.com>
2023-10-04 23:44:47 -0500

    [9.19] Persistent user - Check local storage for token - Set user state accordingly

--- src/App.js
+++ src/App.js
@@ -66,6 +66,10 @@ const App = () => {
     }
     fetchCountries();
 
+    const encodedJwt = localStorage.getItem("token");
+    // check for existing token
+    encodedJwt && setUser(getUser(encodedJwt));
+
     // signalR
     const newConnection = new HubConnectionBuilder()
       .withUrl(hubEndpoint)
